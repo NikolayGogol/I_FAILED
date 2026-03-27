1ace5625872bbc17053949bf024c4de353d8baf1
MykolaCocaCola <k.gogol13@gmail.com>
2026-03-27 11:38:58 +0200

    Add "Select All" switch for email notifications: implement a computed property to toggle all switches, update UI with new "All" option, and refine SCSS for consistent styling.

--- src/components/settings/notify-tabs/email.vue
+++ src/components/settings/notify-tabs/email.vue
@@ -1,5 +1,6 @@
 <script setup>
   import { storeToRefs } from 'pinia'
+  import { computed } from 'vue'
   import { useEmailSettingsStore } from '@/stores/settings/email'
   import '@/styles/components/settings/notify-tabs/tab.scss'
   //
@@ -20,6 +21,15 @@
   ]
   const emailSettingsStore = useEmailSettingsStore()
   const { switches, selectedRadio } = storeToRefs(emailSettingsStore)
+
+  const allSwitch = computed({
+    get: () => switches.value.length > 0 && switches.value.every(item => item.state),
+    set: val => {
+      for (const item of switches.value) {
+        item.state = val
+      }
+    },
+  })
 </script>
 
 <template>
@@ -44,16 +54,26 @@
     </v-radio-group>
     <p class="text-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A cum dicta doloribus error modi! A at consequatur ex ipsa officiis, possimus, praesentium, quaerat recusandae repudiandae vero vitae voluptatibus. Ullam, voluptas?</p>
     <div class="title mt-6">Related to you and your posts</div>
-    <ul>
+    <ul class="mt-3">
+      <li class="cursor-default all-item px-5">
+        <span>All</span>
+        <v-switch
+          v-model="allSwitch"
+          color="primary"
+          density="comfortable"
+          hide-details
+        />
+      </li>
       <li
         v-for="item in switches"
         :key="item.label"
-        class="cursor-default"
+        class="cursor-default px-5"
       >
         <span>{{ item.label }}</span>
         <v-switch
           v-model="item.state"
           color="primary"
+          density="comfortable"
           hide-details
         />
       </li>
