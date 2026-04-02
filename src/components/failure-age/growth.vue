297f94894f546492d934196cfe6d34aa944434d8
MykolaCocaCola <k.gogol13@gmail.com>
2026-04-02 12:14:37 +0300

    Refactor `failure-age` store and components: centralize computed properties, add `Growth` tab, improve stats calculation logic, and update UI for better modularity and scalability.

--- src/components/failure-age/growth.vue
+++ src/components/failure-age/growth.vue
@@ -1,11 +1,54 @@
 <script setup>
+  import { useFailureAgeStore } from '@/stores/failure-age.js'
 
+  const failureAgeStore = useFailureAgeStore()
+  const list = [
+    {
+      label: 'Failure Beginner',
+      age: 20,
+    },
+    {
+      label: 'Wise Stumbler',
+      age: 25,
+    },
+    {
+      label: 'Growth Master',
+      age: 30,
+    },
+    {
+      label: 'Resilience Master',
+      age: 35,
+    },
+    {
+      label: 'Wisdom Keeper',
+      age: 40,
+    },
+    {
+      label: 'Wisdom Sage',
+      age: 50,
+    },
+  ]
 </script>
 
 <template>
-
+  <section class="section mt-6 grow-section">
+    <h3 class="title-section">Growth Milestones</h3>
+    <ul>
+      <li v-for="(step, index) in list" :key="index">
+        <div class="d-flex align-center">
+          <div class="step mr-4">
+            <div v-if="failureAgeStore.totalAgeData > step.age" class="complete">
+              <img alt="" src="../../assets/age/Container.png">
+            </div>
+            <span v-else>{{ index + 1 }}</span>
+          </div>
+          <div class="d-flex flex-column">
+            <h4>{{ step.label }}</h4>
+            <p class="text-description fs-14">Age {{ step.age }}</p>
+          </div>
+        </div>
+        <div v-if="failureAgeStore.totalAgeData > step.age" class="badge">Achieved</div>
+      </li>
+    </ul>
+  </section>
 </template>
-
-<style scoped lang="scss">
-
-</style>
